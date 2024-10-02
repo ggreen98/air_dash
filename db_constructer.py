adcee73e490adc7f8eead13b539e5e04671ac3a9
ggreen98 <68562274+ggreen98@users.noreply.github.com>
2024-10-01 23:40:50 -0600

    working on processor function

--- db_constructer.py
+++ db_constructer.py
@@ -1,31 +1,34 @@
 import pandas as pd
 import os
 
-species = ['voc', 'o3', 'pm', 'met', 'ch4', 'nox']
+#species = ['voc', 'o3', 'pm', 'met', 'ch4', 'nox']
 species = ['voc']
 #sites = ['LUR', 'TFS', 'LNM']
 sites = ['LUR']
 data_path = '/Users/gabegreenberg/Boulder_AIR/CSV_Structer_Clone/data'
-num = 0
-file_lst = []
-for root, dirs, files in os.walk(data_path):
-    for file in files:
-        if file.endswith(".csv"):  # Check if the file is a CSV
-            for site in sites:
-                site_dict = {}
-                for ss in species:
-                    species_dict = {}
-                    file_path_lst = []
-                    if (site in file) & (ss in file) & ('diagnostic' not in root):
-                        file_path = os.path.join(root, file)
-                        file_path_lst.append(file_path)
-                if len(file_path_lst) > 0:
-                    species_dict[ss] = file_path_lst
-            site_dict[site] = species_dict
-            file_lst.append(site_dict)
-    
+paths_lst = []
 
-print(file_lst)
+for root, dirs, files in os.walk(data_path):
+    site_dict = {}
+    for site in sites:
+        species_dict = {}  # Combine this outside the species loop
+        for ss in species:
+            file_path_lst = []
+            for file in files:
+                if file.endswith(".csv") and (site in file) and (ss in file):
+                    file_path = os.path.join(root, file)
+                    file_path_lst.append(file_path)
+            if file_path_lst:  # Only add if the list is non-empty
+                species_dict[ss] = file_path_lst
+        if species_dict:  # Only add if species_dict has entries
+            if site_dict.get(site) is None:  # Avoid duplicates by checking
+                site_dict[site] = species_dict
+                paths_lst.append(site_dict)
+
+print(paths_lst)
+print(len(paths_lst))
+
+#print(file_lst)
             #file_path = os.path.join(root, file)  # Create the full file path
 
 
